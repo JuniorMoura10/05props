603a6600fef826a603e4e82c8290df57e44c39b7
Júnior Moura <geraldomartins10@gmail.com>
2022-07-14 15:50:22 -0300

    props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+class Componente extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Nome: {this.props.informacoes.nome}</p>
+        <p>Idade: {this.props.informacoes.idade}</p>
+        <p>Profissão: {this.props.informacoes.profissao}</p>
+        <p>Filhos: {this.props.informacoes.filhos}</p>
+      </div>
+    );
+  }
+}
+
+//JSON
+var dados = {nome:'Júnior', idade:41, profissao:'dev', filhos:2};
+
+// Dentro do componente, eu crio uma variável e chamo entre chaves outra variável
+ReactDOM.render(
+  <Componente informacoes={dados} />,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
